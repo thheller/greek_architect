c2bb387f7a7b77386836fc4485604771f202c5f0
Thomas Heller <info@zilence.net>
2010-07-03 21:43:59 +0200

    pssst

--- init.rb
+++ init.rb
@@ -2,5 +2,3 @@ require File.dirname(__FILE__) + "/lib/greek_architect.rb"
 
 cassandra = JSON.parse(File.read("#{Rails.root}/config/cassandra.json"))
 GreekArchitect::runtime.configure(cassandra[Rails.env])
-
-puts "Greek'd"
